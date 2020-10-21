a39fe9f09ac6b888a66a6de16aec31033fe90bce
Felipe <schorles21@gmail.com>
2020-10-20 21:29:14 -0300

    login router

--- codar-pizzaria/src/app/app-routing.module.ts
+++ codar-pizzaria/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-/*import { LoginComponent } from './login/login.component';
- */
+
 const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full'},
   { path: 'inicio', component: HomeComponent},
-  /*{ path: 'login', component: LoginComponent}, */
+  { path: 'login', component: LoginComponent},
   { path: 'cadastro', component: RegisterComponent},
 ];
 
